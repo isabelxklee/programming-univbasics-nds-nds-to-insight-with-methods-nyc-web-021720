3acbc864ee1e6f22a70c15854ccafbaf74da07e8
isabel k. lee <hello@isabelklee.co>
2020-02-06 02:48:59 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -28,7 +28,7 @@ def directors_totals(nds)
   row_index = 0
   
   while row_index < nds.length do
-    director_data = directors_database[row_index]
+    director_data = nds[row_index]
     director_name = nds[row_index][:name]
     
     result[director_name] = gross_for_director(director_data)
